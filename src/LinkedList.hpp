b973a077537377f611af6b559445475d83fffeff
Atakan Onol <atakan16@gmail.com>
2019-10-25 21:18:11 +0300

    before cengclass

--- src/LinkedList.hpp
+++ src/LinkedList.hpp
@@ -8,139 +8,289 @@
 template <class T>
 class LinkedList
 {
-    public: // DO NOT CHANGE THIS PART.
-        LinkedList();
-        LinkedList(const LinkedList &obj);
+public: // DO NOT CHANGE THIS PART.
+	LinkedList();
+	LinkedList(const LinkedList& obj);
 
-        ~LinkedList();
+	~LinkedList();
 
-        Node<T> *getFirstNode() const; //
-        Node<T> *getHead() const;
-        Node<T> *getTail() const;
-        int getNumberOfNodes();
-        bool isEmpty(); //
+	Node<T>* getFirstNode() const; //
+	Node<T>* getHead() const;
+	Node<T>* getTail() const;
+	int getNumberOfNodes();
+	bool isEmpty(); //
 
-        void insertAtTheFront(const T &data); //
-        void insertAtTheEnd(const T &data); //
-        void insertAfterGivenNode(const T &data, Node<T> *prev); //
+	void insertAtTheFront(const T& data); //
+	void insertAtTheEnd(const T& data); //
+	void insertAfterGivenNode(const T& data, Node<T>* prev); //
 
-        void removeNode(Node<T> *node);
-        void removeAllNodes();
+	void removeNode(Node<T>* node);
+	void removeAllNodes();
 
-        Node<T> *findNode(const T &data); //
+	Node<T>* findNode(const T& data); //
 
-        void printAllNodes();
-        void printReversed();
+	void printAllNodes();
+	void printReversed();
 
-        LinkedList &operator=(const LinkedList &rhs);
+	LinkedList& operator=(const LinkedList& rhs);
 
-    private: // YOU MAY ADD YOUR OWN UTILITY MEMBER FUNCTIONS HERE.
+private: // YOU MAY ADD YOUR OWN UTILITY MEMBER FUNCTIONS HERE.
 
-    private: // DO NOT CHANGE THIS PART.
-        Node<T> *head;
-        Node<T> *tail;
+private: // DO NOT CHANGE THIS PART.
+	Node<T>* head;
+	Node<T>* tail;
 };
 
 template<class T>
 LinkedList<T>::LinkedList()
 {
-    /* TODO */
+	/* TODO */
+	this->head = new Node<T>();
+	this->tail = new Node<T>();
+
+	this->head->next = this->tail;
+	this->tail->prev = this->head;
+
+	this->head->prev = NULL;
+	this->tail->next = NULL;
 }
 
 template<class T>
-LinkedList<T>::LinkedList(const LinkedList &obj)
+LinkedList<T>::LinkedList(const LinkedList& obj)
 {
-    /* TODO */
+	/* TODO */
+	Node<T>* current = obj.getFirstNode();
+	LinkedList<T> newList;
+	Node<T>* newListTraverse = newList.head;
+
+
+	while (current != obj.tail && current) {
+		newList.insertAtTheEnd(current->element);
+		current = current->next;
+		newListTraverse = newListTraverse->next;
+	}
+
+	newListTraverse->next = NULL;
+	newList.tail->prev = newListTraverse;
 }
 
 template<class T>
 LinkedList<T>::~LinkedList()
 {
-    /* TODO */
+	/* TODO */
+	Node<T> *current = this->getFirstNode();
+	Node<T>* nextNode;
+
+	while (current) {
+		nextNode = current->next;
+		delete current;
+		current = nextNode;
+	}
+
+	this->head = NULL;
+	this->tail = NULL;
 }
 
 template<class T>
-Node<T> * LinkedList<T>::getFirstNode() const
+Node<T>* LinkedList<T>::getFirstNode() const
 {
-    /* TODO */
+	/* TODO */
+	if (head->next == tail) {
+		return NULL;
+	}
+	else {
+		return head->next;
+	}
 }
 
 template<class T>
-Node<T> * LinkedList<T>::getHead() const
+Node<T>* LinkedList<T>::getHead() const
 {
-    /* TODO */
+	/* TODO */
+	return this->head;
 }
 
 template<class T>
-Node<T> * LinkedList<T>::getTail() const
+Node<T>* LinkedList<T>::getTail() const
 {
-    /* TODO */
+	/* TODO */
+	return this->tail;
 }
 
 template<class T>
-int LinkedList<T>::getNumberOfNodes() 
+int LinkedList<T>::getNumberOfNodes()
 {
-    /* TODO */
+	/* TODO */
+	int i = 0;
+	Node <T>* current = this->getHead()->next;
+
+	while (current) {
+		current = current->next;
+		i++;
+	}
+	return i;
 }
 
 template<class T>
 bool LinkedList<T>::isEmpty()
 {
-    /* TODO */
+	/* TODO */
+	if (this->getHead()->next == this->getTail()) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 template<class T>
-void LinkedList<T>::insertAtTheFront(const T &data)
+void LinkedList<T>::insertAtTheFront(const T& data)
 {
-    /* TODO */
+	/* TODO */
+	Node <T>* newNode;
+
+	newNode->element = data;
+
+	if (this->isEmpty()) {
+		newNode->prev = NULL;
+		newNode->next = NULL;
+
+		this->head->next = newNode;
+		this->tail->prev = newNode;
+	}
+	else {
+		newNode->next = this->getFirstNode();
+		this->getFirstNode()->prev = newNode;
+		this->getHead()->next = newNode;
+		newNode->prev = NULL;
+	}
 }
 
 template<class T>
-void LinkedList<T>::insertAtTheEnd(const T &data)
+void LinkedList<T>::insertAtTheEnd(const T& data)
 {
-    /* TODO */
+	/* TODO */
+	Node <T>* newNode = new Node<T>(data, NULL, NULL);
+
+	if (this->isEmpty()) {
+		newNode->prev = NULL;
+		newNode->next = NULL;
+
+		this->head->next = newNode;
+		this->tail->prev = newNode;
+	}
+	else {
+		newNode->prev = this->tail->prev;
+		this->tail->prev->next = newNode;
+		newNode->next = NULL;
+		this->tail->prev = newNode;
+	}
 }
 
 template<class T>
-void LinkedList<T>::insertAfterGivenNode(const T &data, Node<T> *prev)
-{   
-    /* TODO */
-}
-
-template<class T>
-void LinkedList<T>::removeNode(Node<T> *node)
+void LinkedList<T>::insertAfterGivenNode(const T& data, Node<T>* prev)
 {
-    /* TODO */
+	/* TODO */
+	Node<T>* newNode = new Node<T>(data, NULL, NULL);
+
+	/*if prev == last node*/
+	if (prev->next == NULL) {
+		newNode->prev = this->tail->prev;
+		this->tail->prev->next = newNode;
+		newNode->next = NULL;
+		this->tail->prev = newNode;
+	}
+	else {
+		newNode->next = prev->next;
+		newNode->prev = prev;
+		prev->next = newNode;
+		newNode->next->prev = newNode;
+	}
+}
+
+template<class T>
+void LinkedList<T>::removeNode(Node<T>* node)
+{
+	/* TODO */
+	/*Check if first node*/
+	if (head->next == node) {
+		head->next = head->next->next;
+	}
+	/*Check if last node*/
+	else if (tail->prev == node) {
+		node->prev->next = NULL;
+		tail->prev = node->prev;
+	}
+	else {
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+	}
+	delete node;
 }
 
 template<class T>
 void LinkedList<T>::removeAllNodes()
 {
-    /* TODO */
+	/* TODO */
+	this->head->next = this->getTail();
+	this->tail->prev = this->getHead();
 }
 
 template<class T>
-Node<T> * LinkedList<T>::findNode(const T &data)
+Node<T>* LinkedList<T>::findNode(const T& data)
 {
-    /* TODO */
+	/* TODO */
+	Node<T>* tmp = this->head->next;
+
+	while (tmp != tail && tmp != NULL) {
+		if (tmp->element == data) {
+			return tmp;
+		}
+		tmp = tmp->next;
+	}
+
+	return NULL;
 }
 
 template<class T>
 void LinkedList<T>::printAllNodes()
 {
-    /* TODO */
+	/* TODO */
+	Node <T>* current = this->head->next;
+
+	if (current != getTail()) {
+		while (current) {
+			std::cout << current->element << std::endl;
+			current = current->next;
+		}
+	}
 }
 
 template<class T>
 void LinkedList<T>::printReversed()
 {
-    /* TODO */
+	/* TODO */
+	Node <T> current = tail->prev;
+
+	if (current != getHead()) {
+		while (current) {
+			std::cout << current->element << std::endl;
+			current = current->prev;
+		}
+	}
 }
 
 template<class T>
-LinkedList<T> & LinkedList<T>::operator=(const LinkedList &rhs)
+LinkedList<T>& LinkedList<T>::operator=(const LinkedList& rhs)
 {
-    /* TODO */
+	/* TODO */
+	Node <T>* traverse = rhs.getFirstNode();
+	this.removeAllNodes();
+
+	while (traverse != NULL && traverse != rhs.getTail()) {
+		this->insertAtTheEnd(&traverse.element);
+		traverse = traverse->next;
+	}
 }
 
 #endif //LINKED_LIST_HPP
--- src/WebHistory.cpp
+++ src/WebHistory.cpp
@@ -2,42 +2,42 @@
 
 WebHistory::WebHistory()
 {
-    // Does nothing.
+	// Does nothing.
 }
 
 WebHistory::WebHistory(std::string historyText)
 {
 	// history = url timestamp | url timestamp | url timestamp...
-    // urls are string and timestamps are non-negative integers.
-    std::string delimiter = " | ";
+	// urls are string and timestamps are non-negative integers.
+	std::string delimiter = " | ";
 
 	std::string tabInfo;
-    std::string url;
-    std::string timestamp;
-    size_t pos = 0;
+	std::string url;
+	std::string timestamp;
+	size_t pos = 0;
 
-    while (true)
+	while (true)
 	{
-        pos = historyText.find(delimiter);
+		pos = historyText.find(delimiter);
 
-        bool breakTheLoop = (pos == std::string::npos);
+		bool breakTheLoop = (pos == std::string::npos);
 
-        tabInfo = historyText.substr(0, pos);
-        historyText.erase(0, pos + delimiter.length());
+		tabInfo = historyText.substr(0, pos);
+		historyText.erase(0, pos + delimiter.length());
 
-        pos = tabInfo.find(" ");
-        
-        url = tabInfo.substr(0, pos);
-        timestamp = tabInfo.substr(pos + 1, tabInfo.length() - pos);
+		pos = tabInfo.find(" ");
 
-		Node<Tab> *newPage = new Node<Tab>(Tab(url, std::atoi(timestamp.c_str())), NULL, NULL);
-        insertInOrder(newPage);
+		url = tabInfo.substr(0, pos);
+		timestamp = tabInfo.substr(pos + 1, tabInfo.length() - pos);
 
-        if (breakTheLoop)
+		Node<Tab>* newPage = new Node<Tab>(Tab(url, std::atoi(timestamp.c_str())), NULL, NULL);
+		insertInOrder(newPage);
+
+		if (breakTheLoop)
 		{
 			break;
 		}
-    }
+	}
 }
 
 void WebHistory::printHistory()
@@ -52,14 +52,14 @@ void WebHistory::printHistory()
 	}
 	else
 	{
-		Node<Tab> *node = history.getFirstNode();
-		
+		Node<Tab>* node = history.getFirstNode();
+
 		while (node != history.getTail())
 		{
-		    std::cout << "Page: " << node->element.getUrl() << std::endl;
+			std::cout << "Page: " << node->element.getUrl() << std::endl;
 			std::cout << "Last Visited: " << node->element.getTimestamp() << std::endl;
 			std::cout << std::endl;
-		    node = node->next;
+			node = node->next;
 		}
 	}
 	std::cout << "------------------------------------------------" << std::endl;
@@ -69,39 +69,128 @@ void WebHistory::printHistory()
 WebHistory::WebHistory(std::string url, int timestamp)
 {
 	// TODO
+	Node<Tab>* newPage = new Node<Tab>(Tab(url, timestamp), NULL, NULL);
+	newPage->next = NULL;
+	newPage->prev = NULL;
+
+	history.getHead()->next = newPage;
+	history.getTail()->prev = newPage;
 }
 
-void WebHistory::insertInOrder(Node<Tab> *newPage)
+void WebHistory::insertInOrder(Node<Tab>* newPage)
 {
 	// TODO
+	Node<Tab>* current = this->history.getFirstNode();
+
+	/*If empty*/
+	if (current == NULL) {
+		this->history.getHead()->next = newPage;
+		this->history.getTail()->prev = newPage;
+
+		newPage->next = NULL;
+		newPage->prev = NULL;
+	}
+	else {
+		/*if to be first node*/
+		if (newPage->element.getTimestamp() >= this->history.getHead()->next->element.getTimestamp()) {
+			newPage->next = this->history.getHead()->next;
+			newPage->next->prev = newPage;
+			this->history.getHead()->next = newPage;
+		}
+		/*if last node*/
+		else if (newPage->element.getTimestamp() < this->history.getTail()->prev->element.getTimestamp()) {
+			newPage->next = this->history.getTail();
+			newPage->prev = newPage->next->prev;
+
+			this->history.getTail()->prev->next = newPage;
+			this->history.getTail()->prev = newPage;
+		}
+		else {
+			/*if in between*/
+			while (current) {
+				if (newPage->element.getTimestamp() >= current->element.getTimestamp()) {
+					this->history.insertAfterGivenNode(newPage->element, current->prev);
+					break;
+				}
+				current = current->next;
+			}
+		}
+	}
 }
 
 void WebHistory::goToPage(std::string url, int timestamp)
 {
 	// TODO
+	Node<Tab> *newPage = new Node<Tab>;
+	newPage->element.setTimestamp(timestamp);
+	newPage->element.setUrl(url);
+
+	newPage->next = NULL;
+	newPage->prev = NULL;
+	this->insertInOrder(newPage);
 }
 
 void WebHistory::clearHistory()
 {
 	// TODO
+	history.removeAllNodes();
 }
 
 void WebHistory::clearHistory(int timestamp)
 {
 	// TODO
+	Node<Tab>* current = history.getFirstNode();
+
+	while (current != NULL) {
+		if (current->element.getTimestamp() <= timestamp) {
+			this->history.removeNode(current);
+		}
+
+		current = current->next;
+	}
 }
 
-WebHistory WebHistory::operator+(const WebHistory &rhs) const
+WebHistory WebHistory::operator+(const WebHistory& rhs) const
 {
 	// TODO
+	Node<Tab>* hist1 = this->history.getFirstNode();
+	Node<Tab>* hist2 = rhs.history.getFirstNode();
+
+	WebHistory cleanSlate;
+
+	while (hist1) {
+		cleanSlate.insertInOrder(hist1);
+		hist1 = hist1->next;
+	}
+
+	while (hist2) {
+		cleanSlate.insertInOrder(hist2);
+		hist2 = hist2->next;
+	}
+
+	return cleanSlate;
 }
 
 int WebHistory::timesVisited(std::string pageName)
 {
 	// BONUS
+	int i = 0;
+
+	Node<Tab>* current = this->history.getFirstNode();
+
+	while (current) {
+		if (current->element.getWebSite() == pageName) {
+			i++;
+		}
+		current = current->next;
+	}
+
+	return i;
 }
 
 std::string WebHistory::mostVisited()
 {
 	// BONUS
+	/*Later*/
+	return "";
 }
